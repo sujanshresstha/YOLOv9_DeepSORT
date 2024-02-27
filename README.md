a642ec4e9bca0da0638ab28d7ac5570d244cac7f
Sujan Shrestha <techblish@Sujans-MacBook-Pro.local>
2024-02-27 18:53:26 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # YOLOv9_DeepSORT
 This repository contains code for object detection and tracking in videos using the YOLOv9 object detection model and the DeepSORT algorithm.
+
+## Demo 
+<p align="center">
+  <img src="data/helpers/cars.gif" alt="demo" width="45%">
+  <img src="data/helpers/test.gif" alt="blur" width="45%">
+</p>
+
+## Installation
+1. Clone this repository:
+  ```
+   git clone https://github.com/sujanshresstha/YOLOv9_DeepSORT.git
+   cd YOLO-NAS_DeepSORT
+  ```
+
+2. Create new environment using conda
+  ```
+  conda env create -f conda.yml
+  conda activate yolonas-deepsort
+  ```
+
+3. Clone YOLOv9 repository:
+  ```
+   git clone https://github.com/WongKinYiu/yolov9.git
+   pip install -r requirements.txt
+  ```
+
+## Usage
+1. Prepare the video file:
+   - Place the video file in the desired location.
+   - Update the `video` flag in the path of the video file or set it to `0` to use the webcam as the input.
+2. Download YOLOv9 model:
+   - Clone the `model` from YOLOv9. 
+   - Make sure the corresponding model weights are available.
+3. Configure the output video:
+   - Update `output` flag in the code to specify the path and filename of the output video file.
+4. Set the confidence threshold:
+   - Adjust the `conf` flag in the code to set the confidence threshold for object detection. Objects with confidence below this threshold will be filtered out.
+5. If you want to detect and track certain object on video 
+   - Modify the `class_id` flag in the code to specify the class ID for detection. The default value of the flag is set to None. If you wish to detect and track only persons, set it to 0, or refer to the coco.names file for other options.
+6. If you want to blur certain object while tracking
+   - Modify the `bulr_id` flag in the code to specify the class ID for detection. The default value of the flag is set to None. 
+
+7. Run the code:
+   ```
+   # Run object tracking
+   python object_tracking.py --video ./data/video/test.mp4 --output ./output/output.mp4 --model yolo_nas_l
+
+   # Run object tracking on webcam (set video flag to 0)
+   python object_tracking.py --video 0 --output ./output/webcam.mp4 --model yolo_nas_l
+
+   # Run person tracking (set class_id flag to 0 for person)
+   python object_tracking.py --video ./data/video/test.mp4 --output ./output/output.mp4 --model yolo_nas_l --class_id 0
+   
+   # Run tracking on a video with burring certain objects (set blur_id flag to 0 for person)
+   python object_tracking.py --video ./data/video/test.mp4 --output ./output/output.mp4 --model yolo_nas_l --blur_id 0
+   ```
+   
+
+## Acknowledgements
+- This code is built upon the YOLOv9 model and the DeepSort algorithm.
+- Credits to the authors and contributors of the respective repositories used in this project.
+
+## References
+- [Implementation of paper - YOLOv9: Learning What You Want to Learn Using Programmable Gradient Information](https://github.com/WongKinYiu/yolov9/blob/main/README.md)
+- [Simple Online and Realtime Tracking with a Deep Association Metric](https://arxiv.org/abs/1703.07402)
---
+++ object_tracking.py
@@ -0,0 +1,110 @@
+import cv2
+import torch
+import numpy as np
+from absl import app, flags
+from absl.flags import FLAGS
+from deep_sort_realtime.deepsort_tracker import DeepSort
+from models.common import DetectMultiBackend, AutoShape
+
+# Define command line flags
+flags.DEFINE_string('video', './data/test.mp4', 'path to input video')
+flags.DEFINE_string('output', './output/output.mp4', 'path to output video')
+flags.DEFINE_float('conf', 0.50, 'confidence threshold')
+flags.DEFINE_integer('blur_id', None, 'class ID to apply Gaussian Blur')
+flags.DEFINE_integer('class_id', None, 'class ID to blur')
+
+def main(_argv):
+  # Initialize the video capture
+  cap = cv2.VideoCapture(FLAGS.video)
+  frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+  frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+  fps = int(cap.get(cv2.CAP_PROP_FPS))
+  # video writer objects
+  fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+  writer = cv2.VideoWriter(FLAGS.output, fourcc, fps, (frame_width, frame_height))
+
+  # Initialize the DeepSort tracker
+  tracker = DeepSort(max_age=50)
+  # select device (CPU or GPU)
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+  # Load YOLO model
+  model = DetectMultiBackend(weights='./weights/yolov9-e.pt',device=device, data='configs/coco.yaml', fuse=True)
+  model = AutoShape(model)
+
+  # Load the COCO class labels
+  classes_path = "C:/Users/T.Tetsuo/Documents/code/pythonProject/yolov9/coco.names"
+  with open(classes_path, "r") as f:
+      class_names = f.read().strip().split("\n")
+
+  # Create a list of random colors to represent each class
+  np.random.seed(42)
+  colors = np.random.randint(0, 255, size=(len(class_names), 3)) 
+
+  while True:
+      ret, frame = cap.read()
+      if not ret:
+          break
+      # Run model on each frame
+      results = model(frame)
+      results = []
+      for det in results.pred[0]:
+          label, confidence, bbox = det[5], det[4], det[:4]
+          x1, y1, x2, y2 = map(int, bbox)
+          class_id = int(label)
+
+          # Filter out weak detections by confidence threshold and class_id
+          if FLAGS.class_id is None:
+              if confidence < FLAGS.conf:
+                  continue
+          else:
+              if class_id != FLAGS.class_id or confidence < FLAGS.conf:
+                  continue
+
+          results.append([[x1, y1, x2 - x1, y2 - y1], confidence, class_id])
+
+      tracks = tracker.update_tracks(results, frame=frame)
+
+      for track in tracks:
+          if not track.is_confirmed():
+              continue
+          track_id = track.track_id
+          ltrb = track.to_ltrb()
+          class_id = track.get_det_class()
+          x1, y1, x2, y2 = map(int, ltrb)
+          color = colors[class_id]
+          B, G, R = map(int, color)
+          text = f"{track_id} - {class_names[class_id]}"
+
+          cv2.rectangle(frame, (x1, y1), (x2, y2), (B, G, R), 2)
+          cv2.rectangle(frame, (x1 - 1, y1 - 20), (x1 + len(text) * 12, y1), (B, G, R), -1)
+          cv2.putText(frame, text, (x1 + 5, y1 - 8), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+          # Apply Gaussian Blur
+          if FLAGS.blur_id is not None and class_id == FLAGS.blur_id:
+              if 0 <= x1 < x2 <= frame.shape[1] and 0 <= y1 < y2 <= frame.shape[0]:
+                  frame[y1:y2, x1:x2] = cv2.GaussianBlur(frame[y1:y2, x1:x2], (99, 99), 3)
+
+
+      cv2.imshow('YOLOv9 Object tracking', frame)
+      writer.write(frame)
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
+
+  # Release video capture and writer
+  cap.release()
+  writer.release()
+
+if __name__ == '__main__':
+  try:
+      app.run(main)
+  except SystemExit:
+      pass
+
+
+
+
+
+
+
+
